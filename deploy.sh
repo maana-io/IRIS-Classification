cb9e62eda6b668c6cb9411824134b16d9e2056ea
Linh Nguyen <lnguyen@maana.io>
2020-07-28 16:08:01 -0500

    k8s files

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+
+docker build -t classification .
+docker tag classification mkdrservices.azurecr.io/classification:v3.0.36
+docker push mkdrservices.azurecr.io/classification:v3.0.36
+
+kubectl delete service classification
+kubectl delete deployment classification
+
+#kubectl apply -f similarity_pvc.yaml
+kubectl apply -f classification_deployment.yaml
+kubectl apply -f classification_service.yaml
