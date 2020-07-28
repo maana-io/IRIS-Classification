2ba877439b5c399b3870c6ee4dce5dcb97081600
Linh Nguyen <lnguyen@maana.io>
2020-07-28 17:29:16 -0500

    deployment script bug fix

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ docker push mkdrservices.azurecr.io/classification:v3.0.36
 kubectl delete service classification
 kubectl delete deployment classification
 
-#kubectl apply -f similarity_pvc.yaml
+#kubectl apply -f classification_pvc.yaml
 kubectl apply -f classification_deployment.yaml
 kubectl apply -f classification_service.yaml
